bc1fb2742e050bbcc231aeb112fc8070fcfc6ad5
Icarrius <al.tarasov@dats.games>
2024-02-08 19:06:58 +0300

    Base script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+               
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Сначала нужно считать нажатие клавиши (ось Horizontal)
+
+        // Далее изменить скорость Rigidbody (velocity)
+
+        // Добавить остлеживание нажатия пробела (Space)
+       
+    }
+}
