665f017d4e54a5659dadd78e7a8d573ca6e99af5
Севиль Исраелян <sevilisraelan@iMac-Sevil.local>
2024-02-08 20:26:53 +0300

    playeer mo

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-
+    public float speed = 10;
+    public float jampPower = 5;
     // Start is called before the first frame update
     void Start()
     {
-               
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Сначала нужно считать нажатие клавиши (ось Horizontal)
+        float x = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(x * speed, rb.velocity.y) ;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jampPower, ForceMode2D.Impulse);
+            
 
-        // Далее изменить скорость Rigidbody (velocity)
-
-        // Добавить остлеживание нажатия пробела (Space)
-       
-    }
+        }
+     }
 }
