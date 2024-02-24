ae1a3c9507f794abbadd0168c85a1e36d594ee8b
Севиль Исраелян <sevilisraelan@iMac-Sevil.local>
2024-02-24 12:08:33 +0300

    a

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,7 +12,7 @@ public class PlayerMovement : MonoBehaviour
 
     public Vector2 boxSize;
     public float distance;
-
+    private int jampkount;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,23 +25,27 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(x * speed, rb.velocity.y) ;
 
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        if (Input.GetKeyDown(KeyCode.Space) && (IsGrounded() || jampkount <2))
+           
         {
+            rb.velocity = Vector2.zero;
+            jampkount = jampkount + 1; 
             rb.AddForce(Vector2.up * jampPower, ForceMode2D.Impulse);
         }
     }
 
-    // Персонаж на земле?
+    // ???????? ?? ??????
     private bool IsGrounded()
     {
         if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, distance, groundLayer))
         {
-            // Да
+            jampkount = 0;
+            // ??
             return true;
         }
         else
         {
-            // Нет
+            // ???
             return false;
         }
     }
@@ -50,5 +54,6 @@ public class PlayerMovement : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position - transform.up * distance, boxSize);
+
     }
 }
